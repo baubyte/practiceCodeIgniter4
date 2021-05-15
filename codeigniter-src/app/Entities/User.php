ccd011769fa05d5ce06669aab3a71dcb2d7ddb6b
baubyte <paredbaez.martin@gmail.com>
2021-05-15 20:32:55 -0300

    Entidad Usuario

--- codeigniter-src/app/Entities/User.php
+++ codeigniter-src/app/Entities/User.php
@@ -3,8 +3,41 @@
 namespace App\Entities;
 
 use CodeIgniter\Entity;
+use PhpParser\Node\Expr\Cast\String_;
 
 class User extends Entity
 {
-    
+    /**Mutator */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * Filtro para setear el password
+     *
+     * @param string $password
+     * @return void
+     */
+    protected function setPassword(string $password)
+    {
+        $this->attributes['password'] =  password_hash($password, PASSWORD_DEFAULT);
+    }
+    /**
+     * Generar el Usuario con la combinación
+     * del nombre y apellido
+     *
+     * @return void
+     */
+    public function generateUsername()
+    {
+        $this->attributes['username'] = explode(" ", $this->name)[0]. explode(" ", $this->surname)[0];
+    }
+    /**
+     * Filtro por el cual Obtiene el Password
+     *
+     * @return void
+     */
+    protected function getPassword()
+    {
+        return $this->attributes['password'];
+    }
+
 }
