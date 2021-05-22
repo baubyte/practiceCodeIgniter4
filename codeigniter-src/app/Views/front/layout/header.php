52b7d7650dce1006a0adaaea6961b1da8ae8f589
baubyte <paredbaez.martin@gmail.com>
2021-05-22 14:40:13 -0300

    Add Bulma an Icons Offline

--- codeigniter-src/app/Views/front/layout/header.php
+++ codeigniter-src/app/Views/front/layout/header.php
@@ -1,10 +1,27 @@
 <section class="hero is-success">
     <div class="hero-body">
         <p class="title">
-            Mi Blog Personal
+        💀 Bienvenidos a Mi Blog 💀
         </p>
         <p class="subtitle">
-            Lista de Entradas
+            Lista de Entradas 📝  
         </p>
+    </div>
+    <div class="hero-foot">
+        <nav class="tabs is-boxed is-fullwidth">
+            <div class="container">
+                <ul>
+                    <li class="<?=service('request')->uri->getPath() == '/' ? 'is-active':'' ;?>">
+                        <a href="<?=base_url(route_to('home'))?>">Inicio 🏠</a>
+                    </li>
+                    <li class="<?=service('request')->uri->getPath() == 'auth/registro' ? 'is-active':'' ;?>">
+                        <a href="<?=base_url(route_to('register'))?>">Registro 🔏 </a>
+                    </li>
+                    <li class="<?=service('request')->uri->getPath() == 'auth/ingreso' ? 'is-active':'' ;?>">
+                        <a href="<?=base_url(route_to('login'))?>">Ingreso 🔑 </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
 </section>
--- codeigniter-src/app/Views/front/layout/main.php
+++ codeigniter-src/app/Views/front/layout/main.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$this->renderSection('title')?>&nbsp;-&nbsp;Mi Blog</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="stylesheet" href="<?=base_url('assets/bulma/css/bulma.css')?>">
     <!-- Render de CSS -->
     <?=$this->renderSection('css')?>
 </head>
