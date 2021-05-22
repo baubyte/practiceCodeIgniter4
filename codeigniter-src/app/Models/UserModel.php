996db0397134f2f6966d2202d88b24c833b29fe0
baubyte <paredbaez.martin@gmail.com>
2021-05-22 14:38:52 -0300

    Modelo Usuario y Relacion con Informacion de Usuarios

--- codeigniter-src/app/Models/UserModel.php
+++ codeigniter-src/app/Models/UserModel.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-/**Usamos la Entidad Usuario */
+/**Usamos la Entidad Usuario y UserInfo*/
 use App\Entities\User;
+use App\Entities\UserInfo;
 
 class UserModel extends Model
 {
@@ -26,8 +27,45 @@ class UserModel extends Model
 	protected $updatedField         = 'updated_at';
 	protected $deletedField         = 'deleted_at';
 
+	//Fields
 	protected $assignGroup;
+	protected $userInfo;
 
+	//Callbacks
+	protected $beforeInsert = ['addGroup'];
+	protected $afterInsert = ['storeUserInfo'];
+
+
+	/**
+	 * Asigna un grupo al usuario al ser insertado
+	 *
+	 * @param [array] $data
+	 * @return $data
+	 */
+	protected function addGroup($data)
+	{
+		$data['data']['group_id'] = $this->assignGroup;
+		return $data;
+	}
+	protected function storeUserInfo($data)
+	{
+		/**Asignamos el valor del id del usuario que se inserto 
+		 * para luego inserte al información del usuario
+		*/
+		$this->userInfo->user_id = $data['id'];
+		//Llamamos la modelo
+		$userInfoModel = model('UserInfoModel');
+		//Insertamos los datos
+		$userInfoModel->insert($this->userInfo);
+
+	}
+	/**
+	 * Busca el grupo por nombre y 
+	 * lo asigna a la propiedad $assignGroup
+	 * 
+	 * @param string $group
+	 * @return void
+	 */	
 	public function withGroup(string $group)
 	{
 		$result = $this->db->table('groups')
@@ -38,4 +76,8 @@ class UserModel extends Model
 		}
 	}
 
+	public function addUserInfo(UserInfo $userInfo)
+	{
+		$this->userInfo = $userInfo;
+	}
 }
