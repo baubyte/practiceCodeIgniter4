fccbdd40f65289cd7a22d05baf2645f3beefc598
baubyte <paredbaez.martin@gmail.com>
2021-05-23 15:54:02 -0300

    Documentacion y Configuracion de paginacion

--- codeigniter-src/app/Config/Blog.php
+++ codeigniter-src/app/Config/Blog.php
@@ -6,6 +6,18 @@ use CodeIgniter\Config\BaseConfig;
 
 class Blog extends BaseConfig
 {
+	/**
+ 	 * Propiedad de configuración que determina un 
+	 * grupo por defecto para cada usuario nuevo que crea
+	 * @var string
+	 */
 	public $defaultGroupUsers = 'User';
+	/**
+	 * Propiedad de configuración que determina la cantidad
+	 * de registros que se mostraran por pagina
+	 *
+	 * @var integer
+	 */
+	public $regPerPage = 1;
 
 }
