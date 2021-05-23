d73d33c8ed66fc8d71da69290c07f402966af2ad
baubyte <paredbaez.martin@gmail.com>
2021-05-23 17:03:31 -0300

    Editar Categoria

--- codeigniter-src/app/Controllers/Admin/Category.php
+++ codeigniter-src/app/Controllers/Admin/Category.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 use App\Entities\Category as EntitiesCategory;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Category extends BaseController
 {
@@ -14,7 +15,13 @@ class Category extends BaseController
 	 */
 	public function index()
 	{
-		return view('admin/categories');
+		/**Lamamos al Modelo */
+		$categoryModel = model('CategoryModel');
+		/**Obtenemos los registros paginados */
+		$categories = $categoryModel->orderBy('created_at', 'DESC')->paginate(config('Blog')->regPerPage);
+		/**Obtenemos las paginas*/
+		$pager = $categoryModel->pager;
+		return view('admin/category/categories', ['categories' => $categories, 'pager' => $pager]);
 	}
 
 	/**
@@ -25,7 +32,7 @@ class Category extends BaseController
 	 */
 	public function create()
 	{
-		return view('admin/category_create');
+		return view('admin/category/category_create');
 	}
 	/**
 	 * Guarda la categoria
@@ -34,7 +41,7 @@ class Category extends BaseController
 	 */
 	public function store()
 	{
-		if ($this->validateCategory() === true) {
+		if ($this->validateCategoryStore() === true) {
 			/**Entidad Categoria asignamos las propiedades*/
 			$category = new EntitiesCategory($this->request->getPost());
 			/**Lamamos al Modelo */
@@ -42,45 +49,118 @@ class Category extends BaseController
 			/**Insertamos la Categoria */
 			$categoryModel->insert($category);
 			return redirect()->route('categories')->with('msg', [
-                'type' => 'success',
-                'header' => '¡Éxito! 😬',
-                'body'=> 'La Categoria se Guardo Correctamente.'
-                ]);
+				'type' => 'success',
+				'header' => '¡Éxito! 😬',
+				'body' => 'La Categoria se Guardo Correctamente.'
+			]);
 		} else {
 			return redirect()->back()->withInput()->with('msg', [
 				'type' => 'danger',
 				'header' => '¡Ups! 😱',
 				'body' => 'Surgieron Errores.'
-			])->with('errors', $this->validateCategory());
+			])->with('errors', $this->validateCategoryStore());
 		}
 	}
+	public function edit(int $id)
+	{
+		//Llamamos al Modelo
+		$categoryModel = model('CategoryModel');
+		//Buscamos la categoria por el id
+		$category = $categoryModel->find($id);
 
+		if ($category == false) {
+			//si no la encuentra desencadenamos un exenció
+			throw PageNotFoundException::forPageNotFound();
+		} else {
+			return view('admin/category/category_edit', ['category' => $category]);
+		}
+	}
+	public function update()
+	{
+		if ($this->validateCategoryUpdate() === true) {
+			/**Entidad Categoria asignamos las propiedades*/
+			$category = new EntitiesCategory($this->request->getPost());
+			/**Lamamos al Modelo */
+			$categoryModel = model('CategoryModel');
+			/**Insertamos la Categoria */
+			$categoryModel->save($category);
+			return redirect()->route('categories')->with('msg', [
+				'type' => 'success',
+				'header' => '¡Éxito! 😬',
+				'body' => 'La Categoria se Actualizo Correctamente.'
+			]);
+		} else {
+			return redirect()->back()->withInput()->with('msg', [
+				'type' => 'danger',
+				'header' => '¡Ups! 😱',
+				'body' => 'Surgieron Errores.'
+			])->with('errors', $this->validateCategoryUpdate());
+		}
+	}
 	/**
-     * Se encarga de validar el campo de Categoría
-     *
-     * @return boolean
-     */
-    public function validateCategory()
-    {
-        //Llamamos al servicio de validación
-        $validation = service('validation');
-        //Seteamos las reglas
-        $validation->setRules([
-            'name' => [
-                'label'  => 'Nombre',
-                'rules'  => 'required|alpha_space|min_length[3]|max_length[120]',
-                'errors' => [
-                    'required' => 'El {field} es Obligatorio.',
-                    'alpha_space' => 'Solo se permiten letras.',
-                    'min_length' => 'El {field} debe ser mayor a 3 Caracteres.',
+	 * Se encarga de validar el campo de Categoría
+	 * al dar de alta
+	 *
+	 * @return boolean
+	 */
+	public function validateCategoryStore()
+	{
+		//Llamamos al servicio de validación
+		$validation = service('validation');
+		//Seteamos las reglas
+		$validation->setRules([
+			'name' => [
+				'label'  => 'Nombre',
+				'rules'  => 'required|alpha_numeric_punct|min_length[3]|max_length[120]',
+				'errors' => [
+					'required' => 'El {field} es Obligatorio.',
+					'alpha_numeric_punct' => 'Solo se permiten letras y Signos de Puntuación.',
+					'min_length' => 'El {field} debe ser mayor a 3 Caracteres.',
 					'max_length' => 'El {field} debe ser menor a 120 Caracteres.',
 				],
 			],
-        ]);
-        //Comprobamos si pasa la validación
-        if ($validation->withRequest($this->request)->run() === false) {
-            return $validation->getErrors();
-        }
-        return true;
-    }
+		]);
+		//Comprobamos si pasa la validación
+		if ($validation->withRequest($this->request)->run() === false) {
+			return $validation->getErrors();
+		}
+		return true;
+	}
+	/**
+	 * Se encarga de validar el campo de Categoría
+	 * al actualizar
+	 *
+	 * @return boolean
+	 */
+	public function validateCategoryUpdate()
+	{
+		//Llamamos al servicio de validación
+		$validation = service('validation');
+		//Seteamos las reglas
+		$validation->setRules([
+			'name' => [
+				'label'  => 'Nombre',
+				'rules'  => 'required|alpha_numeric_punct|min_length[3]|max_length[120]',
+				'errors' => [
+					'required' => 'El {field} es Obligatorio.',
+					'alpha_numeric_punct' => 'Solo se permiten letras y Signos de Puntuación.',
+					'min_length' => 'El {field} debe ser mayor a 3 Caracteres.',
+					'max_length' => 'El {field} debe ser menor a 120 Caracteres.',
+				],
+			],
+			'id' => [
+				'label'  => 'Identificador',
+				'rules'  => 'required|is_not_unique[categories.id]',
+				'errors' => [
+					'required' => 'El {field} es Obligatorio.',
+					'is_not_unique' => 'El {field} no es Valido.',
+				],
+			],
+		]);
+		//Comprobamos si pasa la validación
+		if ($validation->withRequest($this->request)->run() === false) {
+			return $validation->getErrors();
+		}
+		return true;
+	}
 }
