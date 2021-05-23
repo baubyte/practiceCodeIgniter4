94f605d2187da9ad4361a119f13630c8b4a8c47e
baubyte <paredbaez.martin@gmail.com>
2021-05-22 21:40:48 -0300

    Agregado botones de Dashboard

--- codeigniter-src/app/Views/front/layout/header.php
+++ codeigniter-src/app/Views/front/layout/header.php
@@ -1,25 +1,34 @@
 <section class="hero is-success">
     <div class="hero-body">
         <p class="title">
-        💀 Bienvenidos a Mi Blog 💀
+            💀 Bienvenidos a Mi Blog 💀
         </p>
         <p class="subtitle">
-            Lista de Entradas 📝  
+            Lista de Entradas 📝
         </p>
     </div>
     <div class="hero-foot">
         <nav class="tabs is-boxed is-fullwidth">
             <div class="container">
                 <ul>
-                    <li class="<?=service('request')->uri->getPath() == '/' ? 'is-active':'' ;?>">
-                        <a href="<?=base_url(route_to('home'))?>">Inicio 🏠</a>
+                    <li class="<?= service('request')->uri->getPath() == '/' ? 'is-active' : ''; ?>">
+                        <a href="<?= base_url(route_to('home')) ?>">Inicio 🏠</a>
                     </li>
-                    <li class="<?=service('request')->uri->getPath() == 'auth/registro' ? 'is-active':'' ;?>">
-                        <a href="<?=base_url(route_to('register'))?>">Registro 🔏 </a>
+                    <li class="<?= service('request')->uri->getPath() == 'auth/registro' ? 'is-active' : ''; ?>">
+                        <a href="<?= base_url(route_to('register')) ?>">Registro 🔏 </a>
                     </li>
-                    <li class="<?=service('request')->uri->getPath() == 'auth/ingresar' ? 'is-active':'' ;?>">
-                        <a href="<?=base_url(route_to('login'))?>">Ingreso 🔑 </a>
-                    </li>
+                    <?php if (session()->is_logged) : ?>
+                        <li>
+                            <a href="<?= base_url(route_to('posts')) ?>">Dashboard 📑 </a>
+                        </li>
+                        <li>
+                            <a href="<?= base_url(route_to('signout')) ?>">Salir 📤 </a>
+                        </li>
+                    <?php else : ?>
+                        <li class="<?= service('request')->uri->getPath() == 'auth/ingresar' ? 'is-active' : ''; ?>">
+                            <a href="<?= base_url(route_to('login')) ?>">Ingreso 🔑 </a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </nav>
