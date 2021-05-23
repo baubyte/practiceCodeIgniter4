a71a606af6440bd30641894e2435152093b201cb
baubyte <paredbaez.martin@gmail.com>
2021-05-22 22:29:11 -0300

    Vistas Admin

---
+++ codeigniter-src/app/Views/admin/layout/header.php
@@ -0,0 +1,53 @@
+<section class="hero is-dark">
+    <div class="hero-head">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item">
+                    Blog BAUBYTE
+                    </a>
+                    <span class="navbar-burger" data-target="navbarMenuHeroB">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
+                </div>
+                <div id="navbarMenuHeroB" class="navbar-menu">
+                    <div class="navbar-end">
+                        <a class="navbar-item">
+                            ☠
+                            <?=session('username')?>
+                            ☠
+                        </a>
+                        <a class="navbar-item" href="<?= base_url(route_to('signout')) ?>">Salir 📤 </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="hero-body">
+        <p class="title">
+            💀 Dashboard del Blog 💀
+        </p>
+        <p class="subtitle">
+            Administra tu Blog 📝
+        </p>
+    </div>
+    <div class="hero-foot">
+        <nav class="tabs is-boxed is-fullwidth">
+            <div class="container">
+                <ul>
+                    <li class="<?= service('request')->uri->getPath() == 'admin/articulos' ? 'is-active' : ''; ?>">
+                        <a href="<?= base_url(route_to('posts')) ?>">Artículos 📑</a>
+                    </li>
+                    <li class="<?= service('request')->uri->getPath() == 'auth/registro' ? 'is-active' : ''; ?>">
+                        <a href="<?= base_url(route_to('home')) ?>">Categorías 🌟 </a>
+                    </li>
+                    <li class="<?= service('request')->uri->getPath() == 'auth/registro' ? 'is-active' : ''; ?>">
+                        <a href="<?= base_url(route_to('home')) ?>">Usuarios 😋 </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</section>
--- codeigniter-src/app/Views/admin/posts.php
+++ codeigniter-src/app/Views/admin/posts.php
@@ -1,1 +1,10 @@
-<h1>Listado de Articulos</h1>
+<!-- Extendemos del Layout Principal -->
+<?= $this->extend('admin/layout/main') ?>
+<!-- Seccion Titulo -->
+<?= $this->section('title') ?>
+Lista de Artículos
+<?= $this->endSection() ?>
+<!-- Seccion Contenido -->
+<?= $this->section('content') ?>
+<h1>Lista de Articulos</h1>
+<?= $this->endSection() ?>
