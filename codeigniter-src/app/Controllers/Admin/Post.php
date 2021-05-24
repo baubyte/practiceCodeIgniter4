7f02352ae9b9da8238a12e27ff68f70a4f3bdf37
baubyte <paredbaez.martin@gmail.com>
2021-05-23 21:01:33 -0300

    Metodo Temporal Store

--- codeigniter-src/app/Controllers/Admin/Post.php
+++ codeigniter-src/app/Controllers/Admin/Post.php
@@ -4,10 +4,37 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 
+
 class Post extends BaseController
 {
+	/**
+	 * Devuelve la vista con el listado 
+	 * de todos los artículos
+	 *
+	 * @return view
+	 */
 	public function index()
 	{
-		return view('admin/posts');
+		return view('admin/post/posts');
+	}
+
+	/**
+	 * Devuelve la el formulario de alta de 
+	 * articulo
+	 *
+	 * @return view
+	 */
+	public function create()
+	{
+		/**Lamamos al Modelo */
+		$categoryModel = model('CategoryModel');
+		/**Obtenemos los registros paginados */
+		$categories = $categoryModel->findAll();
+		return view('admin/post/post_create',['categories' => $categories]);
+	}
+
+	public function store()
+	{
+		dd($this->request->getPost());
 	}
 }
