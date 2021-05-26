0ba911e8bdfc3285b4a670c505cc7e79e25f82f1
baubyte <paredbaez.martin@gmail.com>
2021-05-26 19:46:12 -0300

    Add user_id and callback

--- codeigniter-src/app/Models/PostModel.php
+++ codeigniter-src/app/Models/PostModel.php
@@ -15,7 +15,7 @@ class PostModel extends Model
 	protected $returnType           = Post::class;
 	protected $useSoftDeletes        = true;
 	protected $protectFields        = true;
-	protected $allowedFields        = ['title', 'slug', 'body', 'image', 'published_at'];
+	protected $allowedFields        = ['user_id', 'title', 'slug', 'body', 'image', 'published_at'];
 
 	// Dates
 	protected $useTimestamps        = true;
@@ -26,5 +26,45 @@ class PostModel extends Model
 
 	// Callbacks
 	protected $allowCallbacks       = true;
-	protected $afterInsert          = [];
+	protected $afterInsert          = ['storeCategories'];
+	
+	//Protected Attribute
+	protected $categories = [];
+
+	/**
+	 * Insertamos las categorias del post luego de haber sido inserta
+	 * mediate el callback
+	 *
+	 * @param array $data
+	 * @return $data
+	 */
+	protected function storeCategories(array $data)
+	{
+		//comparamos que no sea vació
+		if (! empty($this->categories)) {
+			$categorypostModel = model('CategorypostModel');
+			$categories = [];
+			foreach ($this->categories as $category) {
+				$categories[] = [
+					'category_id' => $category,
+					'post_id'  => $data['id']
+				];
+			}
+			//Hacemos el insert Batch
+			$categorypostModel->insertBatch($categories); 
+		}
+		return $data;
+	}
+
+	/**
+	 * Asigna las categorías al la propiedad
+	 *
+	 * @param array $categories
+	 * @return void
+	 */
+	public function assignCategories(array $categories = [])
+	{
+		$this->categories = $categories;
+	}
 }
+
