f00d87bf89812789fd2de2167521dc6b55da9c48
baubyte <paredbaez.martin@gmail.com>
2021-05-26 23:32:50 -0300

    Listado de post en la home

--- codeigniter-src/app/Controllers/Front/Home.php
+++ codeigniter-src/app/Controllers/Front/Home.php
@@ -9,6 +9,14 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('front/home');
+		
+		/**Lamamos al Modelo */
+		$postModel = model('PostModel');
+		$posts = $postModel->published()->orderBy('created_at', 'DESC')->paginate(config('Blog')->regPerPage);
+		/**Obtenemos las paginas*/
+		$pager = $postModel->pager;
+		//Cargamos el helper para poder usar character_limiter en la vista
+		helper('text');
+		return view('front/home',['posts' => $posts, 'pager' => $pager]);
 	}
 }
