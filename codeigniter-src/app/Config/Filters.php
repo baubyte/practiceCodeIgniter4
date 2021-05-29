0e23a83d6e6289819f6bcdbe4f67d152ff338174
baubyte <paredbaez.martin@gmail.com>
2021-05-29 17:39:06 -0300

    Fitro anti robot

--- codeigniter-src/app/Config/Filters.php
+++ codeigniter-src/app/Config/Filters.php
@@ -31,12 +31,12 @@ class Filters extends BaseConfig
 	 */
 	public $globals = [
 		'before' => [
-			// 'honeypot',
+			'honeypot',
 			'csrf',
 		],
 		'after'  => [
 			'toolbar',
-			// 'honeypot',
+			'honeypot',
 		],
 	];
 
