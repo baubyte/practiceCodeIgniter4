58bae386dd4622881a2d4ee217e912c795dc3782
baubyte <paredbaez.martin@gmail.com>
2021-05-29 17:21:18 -0300

    vista articulo

--- codeigniter-src/app/Views/front/article.php
+++ codeigniter-src/app/Views/front/article.php
@@ -8,18 +8,25 @@
 <?= $this->section('content') ?>
 <section class="section">
     <div class="content">
-        <img src="<?=$post->getLinkImage() ?>" alt="" style="width: 100%;height: 300px;object-fit:cover;">
-        <h1><?=$post->title?></h1>
+        <img src="<?= $post->getLinkImage() ?>" alt="" style="width: 100%;height: 300px;object-fit:cover;">
+        <h1><?= $post->title ?></h1>
         <!-- se puede usar sin getAuthor() lo mismo que para getFullName() -->
-        <h3>Por: <?=$post->author->fullName?></h3>
-        <p>Fecha: <?=$post->published_at->humanize()?></p>
+        <h3>Por: <?= $post->author->fullName ?></h3>
+        <p>Fecha: <?= $post->published_at->humanize() ?></p>
         <div class="tags are-medium">
             <!-- aplicamos lo mismo getCategories() es lo mismo que solo categories -->
-            <?php foreach($post->categories as $category):?>
-                <span class="tag"><?=$category->name?></span>
-            <?php endforeach;?>
+            <?php foreach ($post->categories as $category) : ?>
+                <span class="tag"><?= $category->name ?></span>
+            <?php endforeach; ?>
         </div>
-        <p><?=$post->body?></p>
+        <p><?= $post->body ?></p>
     </div>
+</section>
+<section class="section">
+    <h4>Artículos Relacionados</h4>
+    <!-- // FiltrosPassing Parameter Array -->
+    <?php foreach ($post->categories as $category) : ?>
+        <?= view_cell('\App\Controllers\Front\Home::filter', ['category' => $category->name, 'limit' => 2]) ?>
+    <?php endforeach; ?>
 </section>
 <?= $this->endSection() ?>
